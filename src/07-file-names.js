fe059abee1e2f0afe783935674745af4274e3e50
Roman Yarovoi <roman.and.yarovoi@gmail.com>
2021-04-12 17:00:34 +0300

    feat: implement 07-flie-names task

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,22 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const newArr = [];
+  names.forEach((item, index) => {
+    if (newArr.includes(item)) {
+      let cnt = 0;
+      for (let i = 0; i < index; i++) {
+        if (item === names[i] || item === newArr[i]) {
+          cnt += 1;
+        }
+      }
+      newArr.push(`${item}(${cnt})`);
+    } else {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 }
 
 module.exports = renameFiles;
